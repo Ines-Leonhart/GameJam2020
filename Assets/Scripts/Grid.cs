57166b611c002220cb7ff15e2242881fcbdfd23d
Ines de la Torre <itorrequesada@gmail.com>
2020-02-01 10:57:19 +0100

    Grid generation

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] GameObject cellPrefab;
+    [SerializeField] int rows;
+    [SerializeField] int columns;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var size = cellPrefab.GetComponent<Renderer>().bounds.size;
+        for (int i = -columns / 2; i <= columns / 2; ++i)
+        {
+            for (int j = -rows / 2; j <= rows / 2; ++j)
+            {
+                var go = Instantiate(cellPrefab, transform);
+                var position = go.transform.position;
+                position.x = i * (size.x);
+                position.z = j * (size.z);
+
+                go.transform.position = position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float minDistanceForSwipe;
+
+    Vector3 fingerUpPosition;
+    Vector3 fingerDownPosition;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fingerDownPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            MouseUp();
+        }
+    }
+
+    private void MouseUp()
+    {
+        fingerUpPosition = Input.mousePosition;
+
+        var verticalMovementDistance = Mathf.Abs(fingerDownPosition.y - fingerUpPosition.y);
+        var horizontalMovementDistance = Mathf.Abs(fingerDownPosition.x - fingerUpPosition.x);
+
+        if (verticalMovementDistance > minDistanceForSwipe || horizontalMovementDistance > minDistanceForSwipe)
+        {
+            if (verticalMovementDistance > horizontalMovementDistance)
+            {
+                if (fingerDownPosition.y - fingerUpPosition.y > 0)
+                {
+                    Debug.Log("Swipe down");
+                }
+                else
+                {
+                    Debug.Log("Swipe up");
+                }
+            }
+            else
+            {
+                if (fingerDownPosition.x - fingerUpPosition.x > 0)
+                {
+
+                    Debug.Log("Swipe left");
+                }
+                else
+                {
+                    Debug.Log("Swipe right");
+                }
+            }
+        }
+    }
+}
