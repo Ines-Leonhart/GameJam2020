213c85c83b5c4fb87791aabcd32db285486a64fc
Epsiloth <epsiloth@gmail.com>
2020-02-01 10:36:18 +0100

    Got some smooth movement and some sweet spawners fam

--- Assets/Scripts/enemyMovement.cs
+++ Assets/Scripts/enemyMovement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class enemyMovement : MonoBehaviour
 {
+    public int xDir;
+    public int zDir; //Movement direction variables
+
+    private BoxCollider boxCollider; //BoxCollider of this object
+    private Rigidbody rb; //Rigidbody of this object
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get a component reference to this object's BoxCollider2D
+        boxCollider = GetComponent<BoxCollider>();
+
+        //Get a component reference to this object's Rigidbody2D
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(xDir, 0, zDir);
+    }
+
+    protected void Move(int xDir, int zDir)
+    {
+        rb.MovePosition(transform.position + new Vector3(xDir, 0, zDir) * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "gamePlane")
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move(xDir, zDir);
     }
 }
---
+++ Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public int xDir;
+    public int zDir; //Direction variables
+
+    public GameObject enemy1; //Enemy1 prefab
+
+    private float nextActionTime = 0.0f;
+    public float period = 3f; //Timer variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            GameObject my_enemy1 = (GameObject)Instantiate(enemy1, transform.position, transform.rotation);
+
+            enemyMovement my_enemy1_mov = enemy1.GetComponent<enemyMovement>();
+            
+            my_enemy1_mov.xDir = xDir;
+            my_enemy1_mov.zDir = zDir;
+        }
+    }
+}
