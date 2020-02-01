245d6c261ad07c8fa832e4a7c58d8551fe0dafa2
Ines de la Torre <itorrequesada@gmail.com>
2020-02-01 11:45:37 +0100

    Automatically instantiating the spawners

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
     [SerializeField] GameObject cellPrefab;
     [SerializeField] int rows;
     [SerializeField] int columns;
+    [SerializeField] GameObject enemySpawner;
+    [SerializeField] float spawnerOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,29 @@ public class Grid : MonoBehaviour
                 position.z = j * (size.z);
 
                 go.transform.position = position;
+
+                if (j == -rows / 2)
+                {
+                    InstantiateSpawner(position, new Vector3(0, 0, -spawnerOffset), new Vector3(0, 0, 1));
+                }
+
+                if (i == -columns / 2 || i == columns / 2)
+                {
+                    var offset = new Vector3(i == -columns / 2 ? -spawnerOffset : spawnerOffset, 0, 0);
+                    var direction = new Vector3(i == -columns / 2 ? 1 : -1, 0, 0);
+                    InstantiateSpawner(position, offset, direction);
+                }
             }
         }
+    }
+
+    void InstantiateSpawner(Vector3 initialPosition, Vector3 offset, Vector3 direction)
+    {
+        var spawner = Instantiate(enemySpawner);
+        spawner.transform.position = initialPosition + offset;
+
+        var spawnerCmp = spawner.GetComponent<enemySpawner>();
+        spawnerCmp.xDir = (int)direction.x;
+        spawnerCmp.zDir = (int)direction.z;
     }
 }
--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class enemySpawner : MonoBehaviour
 {
@@ -9,14 +7,8 @@ public class enemySpawner : MonoBehaviour
 
     public GameObject enemy1; //Enemy1 prefab
 
-    private float nextActionTime = 0.0f;
+    private float nextActionTime = 1.0f;
     public float period = 3f; //Timer variables
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
@@ -24,9 +16,9 @@ public class enemySpawner : MonoBehaviour
         if (Time.time > nextActionTime)
         {
             nextActionTime += period;
-            GameObject my_enemy1 = (GameObject)Instantiate(enemy1, transform.position, transform.rotation);
 
-            enemyMovement my_enemy1_mov = enemy1.GetComponent<enemyMovement>();
+            GameObject my_enemy1 = Instantiate(enemy1, transform.position, transform.rotation);
+            enemyMovement my_enemy1_mov = my_enemy1.GetComponent<enemyMovement>();
             
             my_enemy1_mov.xDir = xDir;
             my_enemy1_mov.zDir = zDir;
